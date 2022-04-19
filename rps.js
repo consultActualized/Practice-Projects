46e1a40f33b2879f5e670105003a59433c19ec98
consultactualized <raghavendra.mani@consultactualized.com>
2022-04-19 15:04:00 +0530

    Refined function call and added comments

--- rps.js
+++ rps.js
@@ -1,14 +1,17 @@
-options = ["paper","rock","scissors"];
+const options = ["paper","rock","scissors"];
 
 function player1() {
   return options[rand(3)]
 }
+
 function player2() {
   return options[rand(3)]
 }
+
 function rand(value) {
   return Math.floor(Math.random()*value);
 }
+
 function rpsEval(player1,player2) {
   console.log("player 1 picked: " + player1);
   console.log("player 2 picked: " + player2);
@@ -29,12 +32,14 @@ function rpsEval(player1,player2) {
         result.push("player1",player1) :          //append result
         result.push("player2",player2) :          //append result
         "";
-
-result.length > 1 ?
-  console.log("and the winner is: "+ result[0] + "-->" + result[1]) :
-  console.log(result[0]);
+return result;
 }
 
+// call this function to  run the game and see who wins
 function rps() {
-  rpsEval(player1(),player2());
+  var result = rpsEval(player1(),player2());
+  result.length > 1 ?
+  console.log("and the winner is: "+ result[0] + "-->" + result[1]) :
+  console.log(result[0]);
+
 }
