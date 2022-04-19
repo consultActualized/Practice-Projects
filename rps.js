0758660ea97e1e0f4190a6639e0352bfbc5f1c65
consultactualized <raghavendra.mani@consultactualized.com>
2022-04-19 14:45:42 +0530

    initial commit - RPS game

---
+++ rps.js
@@ -0,0 +1,40 @@
+options = ["paper","rock","scissors"];
+
+function player1() {
+  return options[rand(3)]
+}
+function player2() {
+  return options[rand(3)]
+}
+function rand(value) {
+  return Math.floor(Math.random()*value);
+}
+function rpsEval(player1,player2) {
+  console.log("player 1 picked: " + player1);
+  console.log("player 2 picked: " + player2);
+  var result = [];
+
+  player1 == player2 ?                            // check same options
+    result.push("try again"):                     // return try again
+      player1 == options[0] ?                     // check Player 1 = paper
+        player2 == options[1] ?                   // check player 2 = rock
+        result.push("player1",player1) :          //append result
+        result.push("player2",player2) :          //append result
+      player1 == options[1] ?                     //check player 1 = rock
+        player2 == options[2] ?                   //check player 2 = scissors
+        result.push("player1",player1) :          //append result
+        result.push("player2",player2) :          //append result
+      player1 == options[2] ?                     //check player 1 = scissors
+        player2 == options[0] ?                   //check player 2 = paper
+        result.push("player1",player1) :          //append result
+        result.push("player2",player2) :          //append result
+        "";
+
+result.length > 1 ?
+  console.log("and the winner is: "+ result[0] + "-->" + result[1]) :
+  console.log(result[0]);
+}
+
+function rps() {
+  rpsEval(player1(),player2());
+}
